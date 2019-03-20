501b6d7a07fd8d974c4e95dece075fe52a01f45c
Ryan Lau <rlau8@lion.lmu.edu>
2019-03-20 15:48:52 -0700

    committed cachematrix.R for submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x 
+  setInverse <- function() inv <<- solve(x)
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  mtrx <- x$getInverse()
+  
+  if(!is.null(mtrx)){
+    message("Getting cached data...")
+    return(mtrx)
+  }
+  data <- x$get()
+  mtrx <- solve(data)
+  
+  x$setInverse(mtrx)
+  
+  mtrx
+  
 }
